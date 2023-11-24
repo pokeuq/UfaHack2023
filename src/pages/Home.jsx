4423e83bfa13e2ab9df81b012def4efdeb1a51d9
Bezhan <beerubk74@gmail.com>
2023-11-25 02:05:06 +0500

    added faq

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,31 @@
-import { useState } from 'react'
-import { Dialog } from '@headlessui/react'
-import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import { useState } from "react";
+import { Dialog } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { Link } from "react-router-dom";
 
-import logo from "../assets/images/_Softcode.svg"
+import logo from "../assets/images/_Softcode.svg";
 
-// import mainPic from "../assets/images/main.png"
+import mainPic from "../assets/images/main.png";
 
 const navigation = [
-  { name: 'О нас', href: '#about' },
-  { name: 'Cкачать', href: '#download' },
-]
+  { name: "Вопросы", href: "#questions" },
+  { name: "Cкачать", href: "#download" },
+];
 
 const Home = () => {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
     <div className="bg-white">
-      <header className="absolute inset-x-0 top-0 z-50">
-        <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+      <header className="fixed inset-x-0 top-0 z-50 backdrop-blur-md">
+        <nav
+          className="flex items-center justify-between p-6 lg:px-8"
+          aria-label="Global"
+        >
           <div className="flex lg:flex-1">
             <a href="#" className="-m-1.5 p-1.5">
               <span className="sr-only">Your Company</span>
-              <img
-                className="h-8 w-auto"
-                src={logo}
-                alt=""
-              />
+              <img className="h-8 w-auto max-md:w-40" src={logo} alt="" />
             </a>
           </div>
           <div className="flex lg:hidden">
@@ -41,31 +40,40 @@ const Home = () => {
           </div>
           <div className="hidden lg:flex lg:gap-x-12">
             {navigation.map((item) => (
-              <a key={item.name} href={item.href} className="
+              <a
+                key={item.name}
+                href={item.href}
+                className="
               relative after:bg-gray-900 after:absolute after:h-0.5 after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300 cursor-pointer
               
-              text-sm font-semibold leading-6 text-gray-900">
+              text-sm font-semibold leading-6 text-gray-900"
+              >
                 {item.name}
               </a>
             ))}
           </div>
           <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-            <Link to='/login' href="#" className="text-sm font-semibold leading-6 text-gray-900">
+            <Link
+              to="/login"
+              href="#"
+              className="text-sm font-semibold leading-6 text-gray-900"
+            >
               Войти <span aria-hidden="true">&rarr;</span>
             </Link>
           </div>
         </nav>
-        <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+        <Dialog
+          as="div"
+          className="lg:hidden"
+          open={mobileMenuOpen}
+          onClose={setMobileMenuOpen}
+        >
           <div className="fixed inset-0 z-50" />
           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
               <a href="#" className="-m-1.5 p-1.5">
                 <span className="sr-only">Your Company</span>
-                <img
-                  className="h-8 w-auto"
-                  src={logo}
-                  alt=""
-                />
+                <img className="h-8 w-auto" src={logo} alt="" />
               </a>
               <button
                 type="button"
@@ -91,7 +99,7 @@ const Home = () => {
                 </div>
                 <div className="py-6">
                   <Link
-                    to='/login'
+                    to="/login"
                     className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                   >
                     Войти
@@ -112,17 +120,17 @@ const Home = () => {
             className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             }}
           />
         </div>
         <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
             <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-              Скачать приложение вы можете тут.{' '}
+              Скачать приложение вы можете тут.{" "}
               <a href="#" className="font-semibold text-indigo-600">
                 <span className="absolute inset-0" aria-hidden="true" />
-                :3 
+                :3
               </a>
             </div>
           </div>
@@ -131,8 +139,10 @@ const Home = () => {
               Учись бесплатно, весело и эффективно !
             </h1>
             <p className="mt-6 text-lg leading-8 text-gray-600">
-              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-              fugiat veniam occaecat fugiat aliqua.
+              Инновационная образовательная платформа, предлагающая уникальный
+              опыт интерактивного обучения в формате игры. Мы стремимся сделать
+              обучение увлекательным, простым и легкодоступным для всех
+              возрастов.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
@@ -141,7 +151,10 @@ const Home = () => {
               >
                 Начать
               </a>
-              <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+              <a
+                href="#"
+                className="text-sm font-semibold leading-6 text-gray-900"
+              >
                 Узнать больше <span aria-hidden="true">→</span>
               </a>
             </div>
@@ -155,14 +168,168 @@ const Home = () => {
             className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
             style={{
               clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
             }}
           />
         </div>
       </div>
+
+      <section id='questions'  className="bg-white ">
+        <div className="py-8 px-8 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+          <h2 className="mb-8 text-4xl tracking-tight font-extrabold text-gray-900 ">
+            Frequently asked questions
+          </h2>
+          <div className="grid pt-8 text-left border-t border-gray-200   text-justify">
+            <div className="grid grid-cols-2 gap-10 justify-normal max-md:grid-cols-1">
+              <div className="mb-10">
+                {/* Вопрос 1 */}
+                <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 ">
+                  <svg
+                    className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 "
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                  Как часто обновляется контент на платформе?
+                </h3>
+                <p className="text-gray-500 ">
+                  Наша команда постоянно работает над обновлением контента на
+                  платформе "Softcode". Мы стремимся добавлять новые
+                  увлекательные уроки и задания, чтобы обеспечивать вас новыми
+                  интересными материалами для обучения.
+                </p>
+              </div>
+              <div className="mb-10">
+                <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 ">
+                  <svg
+                    className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 "
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                  Можно ли создавать собственные уроки на платформе ?
+                </h3>
+                <p className="text-gray-500 ">
+                  В данный момент функция создания собственных уроков недоступна
+                  для обычных пользователей. Однако наша команда постоянно
+                  работает над новыми функциями, и мы рассматриваем возможность
+                  добавления этой функции в будущих обновлениях.
+                </p>
+              </div>
+              <div className="mb-10">
+                <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 ">
+                  <svg
+                    className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 "
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                  Есть ли на платформе возможность общения с другими
+                  пользователями?
+                </h3>
+                <p className="text-gray-500 ">
+                  На текущий момент платформа "Softcode" не предусматривает
+                  функции общения между пользователями. Однако мы постоянно
+                  работаем над улучшением возможностей платформы, и мы
+                  рассматриваем возможность добавления этой функции в будущих
+                  обновлениях.
+                </p>
+              </div>
+              <div className="mb-10">
+                <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 ">
+                  <svg
+                    className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 "
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                  Могу ли я использовать платформу "Учимо" на мобильном
+                  устройстве?
+                </h3>
+                <p className="text-gray-500 ">
+                  Да, наша платформа полностью адаптирована для использования на
+                  мобильных устройствах. Вы можете обучаться в любое удобное
+                  время, где бы вы ни находились, используя наше мобильное
+                  приложение.
+                </p>
+              </div>
+              <div className="mb-10">
+                <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 ">
+                  <svg
+                    className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 "
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                  Как можно отслеживать свой прогресс на платформе?
+                </h3>
+                <p className="text-gray-500 ">
+                  Мы предоставляем удобные инструменты для отслеживания вашего
+                  обучения. В вашем профиле вы сможете увидеть свои достижения,
+                  пройденные уроки, результаты тестов и общий прогресс по
+                  выбранной теме.
+                </p>
+              </div>
+              <div className="mb-10">
+                <h3 className="flex items-center mb-4 text-lg font-medium text-gray-900 ">
+                  <svg
+                    className="flex-shrink-0 mr-2 w-5 h-5 text-gray-500 "
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                  Какие возможности есть для администраторов платформы
+                  "Softcode"?
+                </h3>
+                <p className="text-gray-500 ">
+                  Администраторы имеют возможность создавать новые уроки,
+                  добавлять вопросы и тесты, и управлять тематикой обучения. Они
+                  также могут предложить новые идеи для улучшения работы
+                  платформы и функционала для пользователей.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Home
-
+export default Home;
