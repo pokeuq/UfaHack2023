3c25a17151a3528c03d765972220b164b464a0e9
pokeuq <109028724+pokeuq@users.noreply.github.com>
2023-11-26 13:53:43 +0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -143,7 +143,7 @@ const Home = () => {
             </h1>
             <div className="hidden sm:mb-8 sm:mt-8 sm:flex sm:justify-center">
               <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                Скачать приложение вы можете тут.{" "}
+                Скачать приложение вы можете тут{" "}
                 <a href="#download" className="font-semibold text-indigo-600">
                   <span className="absolute inset-0" aria-hidden="true" />
                   :3
